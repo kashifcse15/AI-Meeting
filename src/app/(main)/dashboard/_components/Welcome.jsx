1450c35169ac3cffd6936cec210ab36d54d419c2
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-07-21 20:23:37 +0530

    Changed Welcome page and added Credits also so user can see how much credits is left

--- src/app/(main)/dashboard/_components/Welcome.jsx
+++ src/app/(main)/dashboard/_components/Welcome.jsx
@@ -1,19 +1,49 @@
-"use client"
-import React from 'react'
-import { useUser } from '@/app/components2/auth/provider'
-import Image from 'next/image'
+"use client";
+
+import React from "react";
+import { useUser } from "@/app/components2/auth/provider";
+import Image from "next/image";
+import { Coins } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Welcome = () => {
-    const {user} = useUser();
-  return (
-    <div  className='mt-5 ml-3 mr-3 bg-white p-3 rounded-xl flex justify-between items-center gap-3'> 
-    <div>
-        <h2 className='text-lg font-bold'>Welcome Back, {user?.name}</h2>
-        <p >AI-Driven Interviews, Hassel-free Hiring</p>
-    </div>
-       {user && <Image src={user?.picture} alt="userImage" width={50} height={50} className='rounded-full'/>}
-    </div>
-  )
-}
+  const { user } = useUser();
+  const router=useRouter();
 
-export default Welcome
+  return (
+    <div className="mt-5 mx-3 bg-white rounded-2xl border border-gray-200 shadow-sm px-6 py-5 flex items-center justify-between">
+      {/* Left */}
+      <div>
+        <h2 className="text-2xl font-bold text-gray-900">
+          Welcome Back{user?.name ? `, ${user.name}` : ""} 👋
+        </h2>
+
+        <p className="mt-2 text-gray-500">
+          Ready for your next AI-powered interview? Let's sharpen your skills.
+        </p>
+      </div>
+
+      {/* Right */}
+      <div className="flex items-center gap-5">
+        {user && (
+          <div className="flex items-center gap-2 bg-violet-50 text-violet-700 px-4 py-2 rounded-full font-semibold">
+            <Coins size={18} />
+            <button onClick={()=>router.push("/billing")} className="cursor-pointer">{user.credits} Credits</button>
+          </div>
+        )}
+
+        {user && (
+          <Image
+            src={user.picture}
+            alt="User"
+            width={55}
+            height={55}
+            className="rounded-full border-2 border-violet-500 object-cover"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
--- src/app/billing/page.jsx
+++ src/app/billing/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import { CheckCircle2 } from "lucide-react";
 import { plans } from "@/services/PaymentOptions";
