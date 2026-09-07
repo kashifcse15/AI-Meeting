ccff82666f4fe2cdd8aeec0bdcdc350b67dbb535
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-07 16:09:28 +0530

    Added Consoles to find erros

--- src/app/interview/[interview_id]/start/page.jsx
+++ src/app/interview/[interview_id]/start/page.jsx
@@ -63,6 +63,8 @@ const StartInterview = () => {
           ?.map((item, index) => `${index + 1}. ${item.question}`)
           .join("\n") || "";
 
+          console.log("🔥 INTERVIEW INFO:", interviewInfo);
+console.log("🔥 QUESTION LIST:", questionList);
 
       const assistantOptions = createAssistantOptions(
         interviewInfo,
@@ -70,6 +72,10 @@ const StartInterview = () => {
       );
 
       console.log("🔥 VAPI OPTIONS:", assistantOptions);
+console.log(
+  "🔥 VAPI SYSTEM PROMPT:",
+  assistantOptions?.model?.messages?.[0]?.content
+);
 
       await vapiRef.current.start(assistantOptions);
     }
@@ -102,12 +108,13 @@ const StartInterview = () => {
 
     const handleCallEnd = async () => {
       console.log("🔥 VAPI CALL ENDED");
+        console.log("🔥🔥 CONVERSATION:", conversationRef.current);
       setIsRunning(false);
       toast("Interview Ended");
       // await GenerateFeedback();
     };
     const handleMessage = (message) => {
-
+console.log("🔥 VAPI MESSAGE OBJECT:", message);
       if (message?.conversation) {
         conversationRef.current = message.conversation;
       }
@@ -122,7 +129,8 @@ const StartInterview = () => {
     vapi.on("speech-start", handleSpeechStart);
     vapi.on("speech-end", handleSpeechEnd);
     vapi.on("call-end", handleCallEnd);
-    vapi.on("message", handleMessage)
+    vapi.on("message", handleMessage);
+    vapi.off("error", handleVapiError);
 
     return () => {
       vapi.off("call-start", handleCallStart);
