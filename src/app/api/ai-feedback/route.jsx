a981bbe3b8cef025dbb0756b4c878971219e454f
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-08 20:20:41 +0530

    changed the ai model

--- src/app/api/ai-feedback/route.jsx
+++ src/app/api/ai-feedback/route.jsx
@@ -19,7 +19,7 @@ export async function POST(req) {
     );
 
     const completion = await groq.chat.completions.create({
-      model: "llama-3.3-70b-versatile",
+      model: "openai/gpt-oss-120b",
       messages: [
         {
           role: "user",
