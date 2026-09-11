a0c3b708ea72cb4ce613bbca8754487af5b35c66
kashifcse15 <kashifshaikh.cse@gmail.com>
2026-09-11 15:31:48 +0530

    added mobile css in dashboard

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Welcome from './_components/Welcome'
-import CreateOptions from './_components/CreateOptions'
-import LatestInterview from './_components/LatestInterview'
+import React from "react";
+import Welcome from "./_components/Welcome";
+import CreateOptions from "./_components/CreateOptions";
+import LatestInterview from "./_components/LatestInterview";
 
 const Dashboard = () => {
   return (
-    <div>
-      {/* <Welcome /> */}
-      <h2 className='my-3 font-bold text-2xl'>DASHBOARD</h2>
+    <div className="w-full px-3 sm:px-4 md:px-6">
+      <Welcome />
+
+      <h2 className="my-5 text-2xl font-bold md:text-3xl">
+        DASHBOARD
+      </h2>
+
       <CreateOptions />
+
       <LatestInterview />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
